1ee1cc71eeba6329554d153de6d8d235a3b59520
Jayshil Patel <78102199+Jayshil-Patel@users.noreply.github.com>
2021-04-10 13:04:52 +0000

    Update Signal_Demonstration2.c

--- Signal_Demonstration2.c
+++ Signal_Demonstration2.c
@@ -6,6 +6,8 @@ int main(){
     printf("After 5 seconds I will get a SIGHUP signal\n");
     sleep(5);
     kill(getpid(),1);
+    // generate  signal by instruction using kill system call
+    // Now this process is going to kill itself
     printf("This will not be printed");
     return 0;
 }
