73876bd24e9b0c85ddd0f70ccdd09eb16d0d6f91
Jayshil Patel <78102199+Jayshil-Patel@users.noreply.github.com>
2021-04-10 13:41:27 +0000

    Update Signal_Demonstration5.c

--- Signal_Demonstration5.c
+++ Signal_Demonstration5.c
@@ -5,6 +5,9 @@
 
 void myhandler(int signum){
     printf("\nHey, I got the signal: %d\n\n",signum);
+    // kill -2 /*process ID*/
+    // this kill will not kill the process
+    // insted it will print this message!
 
 }
 
