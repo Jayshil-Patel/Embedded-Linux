6f0ae4a02b65535391b3a16f63e6e44696604a9f
Jayshil Patel <78102199+Jayshil-Patel@users.noreply.github.com>
2021-04-10 13:19:12 +0000

    Update Signal_Demonstration3.c

--- Signal_Demonstration3.c
+++ Signal_Demonstration3.c
@@ -3,12 +3,15 @@
 #include<stdlib.h>
 #include<unistd.h>
 int main(){
+    // changing default action of particular signal
     signal(SIGINT,SIG_IGN);
     signal(SIGINT,SIG_IGN);
     while (1)
     {
         printf("I am in an infinite loop");
         printf("You cannot kill me by SIGINT <2> or SIGFPE <8>\n");
+        //signal number 2, 8 will not be able to kill this process
+        //hence these signals are ignored
         sleep(2);
     }
     return 0;
