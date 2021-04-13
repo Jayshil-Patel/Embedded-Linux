bc9a6d346f83e40439ec7c0037a26b3b40013a24
Jayshil Patel <78102199+Jayshil-Patel@users.noreply.github.com>
2021-04-13 13:51:21 +0530

    Add files via upload

---
+++ Shared Memory/Demonstration/memory_write.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#include<sys/shm.h>
+main()
+{
+	int id;
+	char *p;
+	id=shmget(5,50,IPC_CREAT|0644);
+	p=shmat(id,0,0);
+	printf("enter your message");
+	scanf("%s",p);
+	//printf("%s\n",p);
+}
